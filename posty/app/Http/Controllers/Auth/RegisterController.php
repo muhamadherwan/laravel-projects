ed81211e3e085df1aaaeb75d4b47f6e552684eea
unknown <hello.wanlab@gmail.com>
2021-04-15 16:57:51 +0800

    auth module

--- posty/app/Http/Controllers/Auth/RegisterController.php
+++ posty/app/Http/Controllers/Auth/RegisterController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -23,11 +25,21 @@ class RegisterController extends Controller
             'password' => 'required|confirmed',
             
         ]);
-        dd('store');    
-
-
+        //dd('store');
+        
         // store user
+         User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);   
+
         // sign in the user
+        auth()->attempt($request->only('email','password'));           
+
         // redirect
+        return redirect()->route('dashboard');
+        
     }
 }
